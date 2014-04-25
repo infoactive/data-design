6f57b2477f3485f2a92c19e49aa47fc01aa98d14
trinachi <trinachi@gmail.com>
2014-04-25 16:51:25 -0500

    Fix nav btn

--- js/data-design.js
+++ js/data-design.js
@@ -1,8 +1,8 @@
 $( document ).ready(function() {
-  $(".btn.open").on("click", function () {
+  $(".nav-btn .open").on("click", function () {
     $(".navbar").animate({"left": "0px"}, 400);
   });
-  $(".btn.close").on("click", function () {
+  $(".nav-btn.close").on("click", function () {
     $(".navbar").animate({"left": "-320px"}, 400);
   });
 });
